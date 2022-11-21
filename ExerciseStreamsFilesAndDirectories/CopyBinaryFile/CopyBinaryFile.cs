80ff5b9c0f151d4f58880cbef80e7758c9f02b8f
Ceco <c_vatovski@abv.bg>
2022-11-21 11:30:14 +0200

    Any messege

---
+++ ExerciseStreamsFilesAndDirectories/CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,22 @@
+﻿namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+
+    public class CopyBinaryFile
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+
+            CopyFile(inputFilePath, outputFilePath);
+        }
+
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            byte[] allData = File.ReadAllBytes(inputFilePath);
+            File.WriteAllBytes(outputFilePath, allData);
+        }
+    }
+}
---
+++ ExerciseStreamsFilesAndDirectories/EvenLines/EvenLines.cs
@@ -0,0 +1,51 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            string res = "";
+            using (StreamReader reader = File.OpenText(inputFilePath))
+            {
+                int count = 0;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (count % 2 == 0)
+                    {
+                        string newLine = "";
+                        for (int i = 0; i < line.Length; i++)
+                        {
+                            char c = line[i];
+
+                            if (c == '-' || c == ',' || c == '.' || c == ',' || c == '!' || c == '?')
+                            {
+                                newLine += '@';
+                                continue;
+                            }
+                            newLine += line[i];
+                        }
+                        string[] arr = newLine.Split();
+                        for (int i = arr.Length - 1; i >= 0; i--)
+                        {
+                            res += arr[i] + " ";
+                        }
+                        res += "\n";
+                    }
+                    count++;
+                }
+            }
+            return res;
+        }
+    }
+}
