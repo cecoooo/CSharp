3739bd028509ad91f4458273fc89f4096ba5e01c
Ceco <c_vatovski@abv.bg>
2022-12-11 13:18:27 +0200

    Add new ex

---
+++ ImplementingLinkedList/ImplementingLinkedList/CustomDoublyLinkedList.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using ImplementingLinkedList;
+public class CustomDoublyLinkedList
+{
+	private class Node
+	{
+		public int value;
+		public Node next = null;
+		public Node previous = null;
+		public Node(int value)
+		{
+			this.value = value;
+		}
+	}
+
+	private Node head;
+	private Node tail;
+	private int count;
+	public int Count
+	{
+		get { return count; }
+		set { count = value; }
+	}
+
+	public void AddFirst(int element)
+	{
+		if (this.count == 0)
+		{
+			this.head = this.tail = new Node(element);
+		}
+		else
+		{
+			Node newHead = new Node(element);
+			newHead.next = this.head;
+			this.head.previous = newHead;
+			this.head = newHead;
+		}
+		this.count++;
+	}
+	public void AddLast(int element)
+	{
+		if (this.count == 0)
+			this.head = this.tail = new Node(element);
+		else
+		{
+			Node newTail = new Node(element);
+			newTail.previous = this.tail;
+			this.tail.next = newTail;
+			this.tail = newTail;
+		}
+		this.count++;
+	}
+
+	public int RemoveFirst()
+	{
+		if (this.count == 0)
+			throw new Exception("You cannot remove elements from empty collection.");
+		int first = this.head.value;
+		this.head.next.previous = null;
+		this.head = this.head.next;
+		this.count--;
+		return first;
+	}
+	public int RemoveLast()
+	{
+		if (this.count == 0)
+			throw new Exception("You cannot remove elements from empty collection.");
+		int last = this.tail.value;
+		this.tail.previous.next = null;
+		this.tail = this.tail.next;
+		this.count--;
+		return last;
+	}
+
+	public void ForEach(Func<int, int> func) 
+	{
+		Node node = this.head;
+		for (int i = 0; i < this.count; i++)
+		{
+			node.value = func(node.value);
+			node = node.next;
+		}
+	}
+
+	public int[] ToArray() 
+	{
+		int[] arr = new int[this.count];
+		Node node = this.head;
+		for (int i = 0; i < this.count; i++)
+		{
+			arr[i] = node.value;
+			node = node.next;
+		}
+		return arr;
+	}
+}
---
+++ ImplementingLinkedList/ImplementingLinkedList/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ImplementingLinkedList
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> func = x => x + 1;
+            CustomDoublyLinkedList list = new CustomDoublyLinkedList();
+            list.AddFirst(5);
+            list.AddFirst(6);
+            list.AddLast(7);
+            list.AddFirst(51);
+            list.AddFirst(62);
+            list.AddLast(73);
+            Console.WriteLine(list.RemoveLast()); // 73
+            Console.WriteLine(list.RemoveFirst()); // 62
+            list.ForEach(func);
+            var arr = list.ToArray();
+            foreach (var item in arr)
+            {
+                Console.Write(item + " "); // 52 7 6 8 
+            }
+        }
+    }
+}
