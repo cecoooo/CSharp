3ddaa5f21a84462b6a653f7ae859efd51fe84dd4
Ceco <c_vatovski@abv.bg>
2023-01-06 19:58:33 +0200

    Add Graph implementation project and some older exercises

---
+++ ImplementatingGraph/ImplementatingGraph/DirectedGraph.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ImplementatingGraph;
+
+
+// Implemented with Adcacency List
+public class DirectedGraph
+{
+    List<List<int>> list;
+    int countIOfNodes;
+
+    public DirectedGraph(int v)
+    {
+        this.countIOfNodes = v;
+        list = new List<List<int>>();
+        for (int i = 0; i < v; i++)
+        {
+            list.Add(new List<int>());
+        }
+    }
+
+    public void CreateEdge(int v, int u)
+    {
+        if (v != u && !list[v].Contains(u))
+            list[v].Add(u);
+    }
+
+    public void PrintGraphConnections()
+    {
+        for (int i = 0; i < this.list.Count; i++)
+        {
+            Console.Write($"Node {i} connected to: ");
+            for (int j = 0; j < this.list[i].Count; j++)
+            {
+                Console.Write(list[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ImplementatingGraph/ImplementatingGraph/UndirectedGraph.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ImplementatingGraph;
+
+
+// Implemented with Matrix
+public class UndirectedGraph<T>
+{
+    private T[][] arr = null;
+    public int countOfElements { get; private set; }
+
+    public UndirectedGraph(int v)
+    {
+        this.countOfElements = v;
+        arr = new T[v][];
+        for (int i = 0; i < v; i++)
+            this.arr[i] = new T[this.countOfElements];
+    }
+
+    public void AddNode(int u, int v, T value)
+    {
+        arr[u - 1][v - 1] = value;
+        arr[v - 1][u - 1] = value;
+    }
+
+    public void PrintGraph()
+    {
+        for (int i = 0; i < this.countOfElements; i++)
+        {
+            for (int j = 0; j < this.countOfElements; j++)
+            {
+                if (i != j)
+                    Console.WriteLine($"Path between Node {i + 1} & Node {j + 1} - {arr[i][j]}");
+            }
+        }
+    }
+}
