a69be101e5f07591e173e22c33419d4e115ff882
Ceco <c_vatovski@abv.bg>
2023-01-07 23:11:29 +0200

    mess

--- ImplementatingGraph/ImplementatingGraph/UndirectedGraph.cs
+++ ImplementatingGraph/ImplementatingGraph/UndirectedGraph.cs
@@ -35,4 +35,46 @@ public class UndirectedGraph<T>
             }
         }
     }
+
+    public bool IsBipartiteGraph() 
+    {
+        bool isBipartite = true;
+        List<int> redGroup = new List<int>();
+        List<int> blueGroup = new List<int>();
+        List<int> stillChecked = new List<int>();
+        int element = -1;
+
+        for (int i = 0; i < this.arr.Length; i++)
+        {
+            bool breakOrNot = false;
+            for (int j = 0; j < this.arr.Length; j++)
+            {
+                if (this.arr[i][j] != null)
+                {
+                    breakOrNot = true;
+                    element = i;
+                    break;
+                }
+            }
+            if (breakOrNot)
+                break;
+        }
+        List<int> curruntList = new List<int>();
+        int offset = -1;
+        while (stillChecked.Count != this.arr.Length && element > -1) {
+            element=curruntList[offset];
+            curruntList = stillChecked.Count % 2 == 0 ? redGroup : blueGroup;
+            if (!curruntList.Contains(element)) curruntList.Add(element);
+            else { isBipartite = false; break; }
+            if (stillChecked.Contains(element)) continue;
+            else stillChecked.Add(element);
+            for (int i = 0; i < this.arr.Length; i++)
+                if (this.arr[element][i] != null)
+                    curruntList.Add(i);
+            offset+=
+
+
+        }
+        return isBipartite;
+    }
 }
