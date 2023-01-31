d1c49ba07d7a46175fd8cb29e14101c3078dc785
Ceco <c_vatovski@abv.bg>
2023-01-31 13:33:17 +0200

    mess

--- ImplementatingGraph/ImplementatingGraph/StartUp.cs
+++ ImplementatingGraph/ImplementatingGraph/StartUp.cs
@@ -6,15 +6,6 @@ namespace ImplementatingGraph
     {
         static void Main(string[] args)
         {
-            //Graph<string> graph = new Graph<string>(5);
-            //graph.AddNode(2, 1, "Added");
-            //graph.AddNode(2, 5, "Added");
-            //graph.AddNode(2, 4, "Added");
-            //graph.AddNode(3, 5, "Added");
-            //graph.AddNode(1, 4, "Added");
-            //graph.AddNode(1, 3, "Added");
-            //graph.PrintGraph();
-
             DirectedGraph graph = new DirectedGraph(5);
             graph.CreateEdge(0, 3);
             graph.CreateEdge(1, 0);
---
+++ ImplementatingGraph/ImplementatingGraph/Tree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using ImplementatingGraph;
+
+public class Tree
+{
+    private class Node
+    {
+        private int value;
+        private List<Node> childs;
+        private Node parrentNode;
+        private List<Node> siblings;
+
+
+
+        public Node(int value)
+        {
+            this.value = value;
+            this.childs = new List<Node>();
+        }
+
+        public void AddChild(Node node)
+        {
+            this.childs.Add(node);
+        }
+    }
+    private Node root;
+    private int height;
+    private List<Node> leafs;
+
+    public Tree(int item)
+    {
+
+    }
+
+}
