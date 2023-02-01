207bf9edfd6d0343625fe3ae28a09ef38c42d95c
Ceco <c_vatovski@abv.bg>
2023-02-01 13:04:15 +0200

    mess

---
+++ HashTable/HashTable/MyHashTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+using HashTable;
+
+public class MyHashTable
+{
+    private string[] data;
+	private int count;
+
+	public int Count
+	{
+		get { return count; }
+		private set { count = value; }
+	}
+
+	public MyHashTable(int length) 
+	{
+		this.data = new string[length];
+		this.count = 0;
+	}
+
+	public void Add(string key, string value) 
+	{
+		this.data[Hashing(key)] = value;
+		count++;
+	}
+
+	public string GetValue(string key) 
+	{
+		return this.data[Hashing(key)];
+	}
+
+	public void DeleteValue(string key) 
+	{
+		this.data[Hashing(key)] = null;
+	}
+
+	public string PrintMyHashTable() 
+	{
+		string res = string.Empty;
+		for (int i = 0; i < this.data.Length; i++)
+		{
+			res += $"[{i}] -> {this.data[i]}\n";
+		}
+		return res;
+	}
+
+	private int Hashing(string element) 
+	{
+		return element.Length % this.data.Length;
+	}
+}
---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyHashTable hashTable = new MyHashTable(10);
+            Console.WriteLine(hashTable.Count);
+            hashTable.Add("Micho", "Kosio");
+            hashTable.Add("icho", "Losio");
+            hashTable.Add("Mic", "Mosio");
+            hashTable.Add("Micdho", "Nosio");
+            hashTable.Add("Micddho", "Posio");
+            Console.WriteLine(hashTable.Count);
+            Console.WriteLine(hashTable.GetValue("Micho"));
+            hashTable.DeleteValue("icho");
+            Console.WriteLine(hashTable.PrintMyHashTable());
+        }
+    }
+}
