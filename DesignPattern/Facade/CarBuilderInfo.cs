7818cbe102c475fa45e1e5aaf2cfa0861e7defbb
Ceco <c_vatovski@abv.bg>
2023-07-06 12:55:04 +0300

    Faceade

---
+++ DesignPattern/Facade/CarBuilderInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    public class CarBuilderInfo:CarBuilderFacade
+    {
+        public CarBuilderInfo(Car car) 
+        {
+            Car = car;
+        }
+        public CarBuilderInfo WithType(string type) 
+        {
+            Car.Type = type;
+            return this;
+        }
+        public CarBuilderInfo WithColor(string color) 
+        {
+            Car.Color = color;
+            return this;
+        }
+        public CarBuilderInfo WithNumberDoors(int number) 
+        {
+            Car.NumberOfDoors = number;
+            return this;
+        }
+    }
+}
---
+++ DesignPattern/Facade/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using Facade;
+
+var car = new CarBuilderFacade().Info.WithType("BMW").WithColor("Black").WithNumberDoors(4).Built.InCity("Munich").AtAddress("ala bala").Build();
+
+Console.WriteLine(car);
