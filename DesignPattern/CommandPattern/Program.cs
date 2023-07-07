a66a6f3eaec14ef73471fbd71bb6fa6221902f4a
Ceco <c_vatovski@abv.bg>
2023-07-07 19:09:29 +0300

    command pattern

---
+++ DesignPattern/CommandPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern
+{
+    public class Program
+    {
+        public static void Main() 
+        {
+            var modifyPrice = new ModifyPrice();
+            var product = new Product("Phone", 500);
+
+            Execute(product, modifyPrice, new ProductCommand(product, PriceAction.Increase, 100));
+            Execute(product, modifyPrice, new ProductCommand(product, PriceAction.Increase, 50));
+            Execute(product, modifyPrice, new ProductCommand(product, PriceAction.Decrease, 25));
+            Console.WriteLine(product);
+        }
+
+        private static void Execute(Product product, ModifyPrice modifyPrice, ICommand productCommand) 
+        {
+            modifyPrice.SetCommand(productCommand);
+            modifyPrice.Invoke();
+        }
+    }
+}
