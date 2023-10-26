5d54c492b8f15fad2e5d48241e16aa0913e052e9
Ceco <c_vatovski@abv.bg>
2023-10-26 19:43:00 +0300

    mess

---
+++ ADOdotNET/IncreaseMinionAge/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+
+string connectionString = "Server=CECO\\SQLEXPRESS;Database=MinionsDB;User Id=sa;Password=1234;TrustServerCertificate=True;";
+
+using SqlConnection connection = new SqlConnection(connectionString);
+connection.Open();
+
+string query = "SELECT [Name], Age\r\nFROM Minions;";
+string query2 = "UPDATE Minions\r\nSET [Name] = LOWER([Name]), Age += 1\r\nWHERE Id = @Id";
+
+int[] ids = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+using SqlCommand command2 = new SqlCommand(query2, connection);
+for (int i = 0; i < ids.Length; i++)
+{
+    command2.Parameters.AddWithValue("@Id", ids[i]);
+    using SqlDataReader reader2 = command2.ExecuteReader();
+}
+
+using SqlCommand command = new SqlCommand(query, connection);
+using SqlDataReader sqlDataReader = command.ExecuteReader();
+
+while (sqlDataReader.Read()) 
+{
+    Console.WriteLine($"{sqlDataReader["Name"]} {sqlDataReader["Age"]}");
+}
---
+++ ADOdotNET/PrintAllMinionNames/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+
+string connectionString = "Server=CECO\\SQLEXPRESS;Database=MinionsDB;User Id=sa;Password=1234;TrustServerCertificate=True;";
+
+using SqlConnection connection = new SqlConnection(connectionString);
+connection.Open();
+
+string query = "SELECT [Name] FROM Minions;";
+
+SqlCommand command = new SqlCommand(query, connection);
+SqlDataReader reader = command.ExecuteReader();
+List<string> list = new List<string>();
+
+while (reader.Read()) 
+{
+    list.Add(reader["Name"].ToString());
+}
+
+for (int i = 0; i < list.Count/2; i++)
+{
+    Console.WriteLine(list[i]);
+    Console.WriteLine(list[list.Count-i-1]);
+}
